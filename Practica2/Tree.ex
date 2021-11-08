219b5c6b36e88a9471b07e4a457533ffc896f285
Esjag <marioagd@ciencias.unam.mx>
2021-11-07 18:18:32 -0600

    Se implemento mandar mensaje a los hijos.
    Cada proceso, manda mensaje a sus dos hijos (árbol binario), si existen, hasta encontrar las hojas del árbol.
    
    Funciones: broadcast, loop
    Módulo: Tree
    Fecha: 7 de noviembre de 2021
    Para la asignatura Computación Distribuida 2022-1.

--- Practica2/Tree.ex
+++ Practica2/Tree.ex
@@ -7,9 +7,7 @@ defmodule Tree do
   defp loop() do
     receive do
       {:broadcast, tree, i, caller} ->
-        IO.puts("Hola #{inspect caller} soy #{inspect self()}")
         broadcast(tree, i)
-        :ok
       {:convergecast, tree, i, caller} -> :ok #Aquí va su código.
     end
   end
@@ -23,7 +21,6 @@ defmodule Tree do
   end
 
   def broadcast(tree, n) do
-    IO.puts("Soy #{inspect self()}")
     # Para simplificar y poder obtener de manera más fácil los hijos del árbol, debemos obtener la llave en el
     # Map que guarda el valor del PID.
     node = Enum.filter(0..n, fn x -> Map.get(tree, x) == self() end)
@@ -31,6 +28,31 @@ defmodule Tree do
     # la raíz.
     if node == [] do
       send(Map.get(tree, 0), {:broadcast, tree, n, self()})
+    else
+      # Obtiene el valor del nodo.
+      node = List.first(node)
+      # Obtiene el indice que se corresponde al hijo izquierdo.
+      hijoI = (2 * node) + 1
+      # Verifica que tenga hijo izquierdo.
+      if hijoI < n do
+        # Obtiene el proceso que se corresponde al hijo izquierdo.
+        hijoI = Map.get(tree, hijoI)
+        # Le manda mensaje a su hijo izquierdo.
+        send(hijoI, {:broadcast, tree, n, self()})
+      end
+      # Obtiene el indice que se corresponde al hijo derecho.
+      hijoD = (2 * node) + 2
+      # Verifica que tenga hijo derecho.
+      if hijoD < n do
+        # Obtiene el proceso que se corresponde al hijo derecho.
+        hijoD = Map.get(tree, hijoD)
+        # Le manda mensaje a su hijo derecho.
+        send(hijoD, {:broadcast, tree, n, self()})
+      end
+      # Si no tuvo ni hijo izquierdo, ni hijo derecho, significa que es hoja.
+      if hijoD >= n && hijoI >= n do
+        IO.puts("El nodo #{inspect node} con proceso #{inspect self()} es hoja.")
+      end
     end
     :ok
   end
